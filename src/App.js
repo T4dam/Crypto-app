8d317e19cec9f5753d65baadd3f4c478f0911b57
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-19 23:03:39 +0300

    remove header

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ console.log(filteredCoins)
     <div className='flex flex-col mt-[64px]'>
       <div className='mb-[64px] flex flex-col justify-center items-center'>
           <Urls />
-          <h1 className=' text-4xl font-black mb-[50px]'>100 populiariausių kripto valiutų</h1>
+          {/* <h1 className=' text-4xl font-black mb-[50px]'>100 populiariausių kripto valiutų</h1> */}
           <h1 className='text-xl mb-[10px]'>Valiutos paieška</h1>
 
       <form>
